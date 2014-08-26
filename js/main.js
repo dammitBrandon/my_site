0431c03b8cc3c8bcfe1fbeff8c7404eb9cb10ccf
btbailey <brandon.bailey@slalom.com>
2014-08-26 07:25:51 -0500

    Integration of smint for desktop completed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-$().ready(function () {
+$(document).ready(function () {
   var screenWidth = window.innerWidth;
   
+// grabs the right picture depending on the screen width
   if (screenWidth <= 480) {
     $('#home-page-image').attr('src', 'images/me4.jpg');
   }
@@ -9,4 +10,14 @@ $().ready(function () {
   } else {
     $('#home-page-image').attr('src', 'images/me2.jpg');
   }
+
+// adds and removes .active from menu links
+  $('nav#nav ul li a').click(function(e) {
+    var $this = $(this);
+    if (!$this.parent().hasClass('active')) { $this.parent().addClass('active').siblings().removeClass('active');}
+  });
+  
+  $('.navbar-main').smint({
+    'scrollSpeed': 1000
+  });
 });

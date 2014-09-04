19f3fc4548ccf057026be300f8a538552d7e6c05
btbailey <brandon.bailey@slalom.com>
2014-09-04 15:44:20 -0500

    WIP integration of scrollspy

--- js/main.js
+++ js/main.js
@@ -12,12 +12,12 @@ $(document).ready(function () {
   }
 
 // adds and removes .active from menu links
-  $('nav#nav ul li a').click(function(e) {
-    var $this = $(this);
-    if (!$this.parent().hasClass('active')) { $this.parent().addClass('active').siblings().removeClass('active');}
-  });
+//  $('nav#nav ul li a').click(function(e) {
+//    var $this = $(this);
+//    if (!$this.parent().hasClass('active')) { $this.parent().addClass('active').siblings().removeClass('active');}
+//  });
   
-  $('.navbar-main').smint({
-    'scrollSpeed': 1000
-  });
+//  $('.navbar-main').smint({
+//    'scrollSpeed': 1000
+//  });
 });
